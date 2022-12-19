38edb7384d988aa1a9a2ca4214e1d1692832cc67
jaydeepkale <Jaynkale@gmail.com>
2022-12-19 08:02:58 -0800

    second version added element to AL and displayed it

--- MyProject/src/com/velocity/Example.java
+++ MyProject/src/com/velocity/Example.java
@@ -7,7 +7,7 @@ public class Example {
 	public static void main(String[] args) {
 		ArrayList<Integer> al = new ArrayList();
 		al.add(10);
-		al.add(20);
+		al.add(30);
 		System.out.println(al);
 	}
 
