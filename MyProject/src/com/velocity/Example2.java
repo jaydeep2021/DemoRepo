f288d26f5c32e641a5eea10d141a0c8f8e3d407a
jaydeepkale <Jaynkale@gmail.com>
2022-12-19 23:37:58 -0800

    Added example2 on premaster

---
+++ MyProject/src/com/velocity/Example2.java
@@ -0,0 +1,14 @@
+package com.velocity;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Example2 {
+
+	public static void main(String[] args) {
+    Map map=new HashMap<>();
+    map.put(1, 10);
+    map.put(2, 20);
+	}
+
+}
