57a3902b140879ba2da9b895ecf820075d96a324
jaydeepkale <Jaynkale@gmail.com>
2022-12-19 23:34:58 -0800

    Added Example1

---
+++ MyProject/src/com/velocity/Example1.java
@@ -0,0 +1,14 @@
+package com.velocity;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+    Set<Integer> s=new HashSet<>();
+    s.add(10);
+    s.add(20);
+	}
+
+}
